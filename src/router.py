61873e4445cbc1e473ead2ef77d42bf0749f8d5b
Isabelle <meguminperson@gmail.com>
2024-05-06 21:42:15 +0100

    feat: Basic router setup

---
+++ src/router.py
@@ -0,0 +1,29 @@
+from contextlib import asynccontextmanager
+from typing import List, Tuple
+
+from fastapi import APIRouter, FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from routers.ip_memes.router import router as ip_memes_router
+
+
+@asynccontextmanager
+async def lifecycle(app: FastAPI):
+    yield
+
+
+app = FastAPI(
+    lifespan=lifecycle,
+)
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+routers: List[Tuple[APIRouter, str]] = [
+    (ip_memes_router, "/"),
+]
+
+
+for router, prefix in routers:
+    app.include_router(router, prefix=prefix)
