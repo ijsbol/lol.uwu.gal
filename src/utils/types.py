8774d5f12fe49165b007da72a1060941c0290bbf
Isabelle <meguminperson@gmail.com>
2024-05-07 14:00:10 +0100

    feat: Updated convig

---
+++ src/utils/types.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from typing import Literal, TypedDict
+
+
+class IPAPI_IPInformation_fail(TypedDict):
+    status: Literal["fail"]
+    message: Literal["private range", "reserved range", "invalid query"]
+
+
+class IPAPI_IPInformation_success(TypedDict):
+    status: Literal["success"]
+    country: str
+    region: str
+    regionName: str
+    city: str
+    zip: str
+    lat: float
+    long: float
+
+
+class IPInformation(TypedDict):
+    country: str
+    region: str
+    regionName: str
+    city: str
+    zip: str
+    lat: str
+    long: str
+
+
+class IPMemes(TypedDict):
+    file_location: str
+    frames: IPMemes_frames
+    position: IPMemes_position
+    text: IPMemes_text
+
+
+class IPMemes_frames(TypedDict):
+    start: int
+    end: int
+
+
+class IPMemes_position(TypedDict):
+    x: int
+    y: int
+
+
+class IPMemes_text(TypedDict):
+    size: int
+    colour: str
+    location: str
+    thickness: int
