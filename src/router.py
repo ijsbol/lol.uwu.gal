53cb2d7fbc3c248a34af8b164929d012e40a449b
Isabelle <meguminperson@gmail.com>
2024-05-07 14:49:53 +0100

    feat: Kawaii error handling

--- src/router.py
+++ src/router.py
@@ -1,8 +1,12 @@
 from contextlib import asynccontextmanager
-from typing import List, Tuple
+from typing import Any, List, Tuple
 
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.templating import (
+    _TemplateResponse,  # pyright: ignore[reportPrivateUsage]
+)
 
 from routers.ip_memes.router import router as ip_memes_router
 
@@ -12,9 +16,8 @@ async def lifecycle(app: FastAPI):
     yield
 
 
-app = FastAPI(
-    lifespan=lifecycle,
-)
+app = FastAPI(lifespan=lifecycle)
+templates = Jinja2Templates(directory="templates")
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -27,3 +30,21 @@ routers: List[Tuple[APIRouter, str]] = [
 
 for router, prefix in routers:
     app.include_router(router, prefix=prefix)
+
+
+@app.exception_handler(404)
+async def custom_404_handler(request: Request, *_: Any, **__: dict[str, Any]) -> _TemplateResponse:
+    return templates.TemplateResponse(
+        name="404.jinja",
+        context={"request": request},
+        status_code=404,
+    )
+
+
+@app.exception_handler(503)
+async def custom_503_handler(request: Request, *_: Any, **__: dict[str, Any]) -> _TemplateResponse:
+    return templates.TemplateResponse(
+        name="503.jinja",
+        context={"request": request},
+        status_code=503,
+    )
